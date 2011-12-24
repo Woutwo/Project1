119aa6b574e6dc22b11f2187755bed7d612b0e7d
Robin Sonneveld <gmfreaky@gmail.com>
2011-12-24 13:57:16 +0100

    camera beweging en shizzle

--- src/nl/wouterenrobin/project1/TestState.java
+++ src/nl/wouterenrobin/project1/TestState.java
@@ -1,5 +1,7 @@
 package nl.wouterenrobin.project1;
 
+import org.lwjgl.input.Keyboard;
+
 public class TestState extends State
 {
 	public float f;
@@ -7,12 +9,19 @@ public class TestState extends State
 	{
 		super.Update();
 		f+=1;
-		Camera.x=(float) Math.cos(f/10)*32;
-		Camera.y=(float) Math.sin(f/10)*32;
+		if (Keyboard.isKeyDown(Keyboard.KEY_UP))
+		{
+			Camera.y--;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN))
+		{
+			Camera.y++;
+		}
 	}
 	public void Draw()
 	{
 		super.Draw();
+		Images.test.draw(-200f, -200f,1600f,1600f);
 		Images.test.draw(32f, 32f);
 	}
 }
